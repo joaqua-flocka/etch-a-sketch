c432ea3d8269040cded39721b71f2f922ead2eb2
Joaquin <joaquinvaldez101@gmail.com>
2022-08-04 01:01:40 -0600

    Fix divs so they have height, clean up code

--- script.js
+++ script.js
@@ -5,10 +5,6 @@ function createGrid(number){
         const newDiv = document.createElement('div');
         newDiv.style.display = 'flex';
         newDiv.style.flex = '1 1 auto';
-        newDiv.style.padding = '1rem';
-        newDiv.style.minHeight = '1px';
-//        newDiv.style.backgroundColor = 'blue';
-//        newDiv.classList.add('square');
         createColumns(number, newDiv);
         container.appendChild(newDiv);
     }
@@ -18,20 +14,17 @@ function createColumns(number, parent){
     for(let i = 0; i < number; i++){
         const newDiv = document.createElement('div');
         newDiv.style.flex = '1 1 auto';
-        newDiv.style.minWidth = '1px';
-        newDiv.style.display = 'flex';
         newDiv.classList.add('square');
 //        newDiv.textContent = 's'
         parent.appendChild(newDiv);
     }
 }
 
-createGrid(16);
+createGrid(160);
 
 let grid = document.querySelectorAll('.square');
 grid.forEach((square) => {
     square.addEventListener('mouseenter', () => {
         square.classList.add('hovered');
-        console.log(square.classList);
     })
 })
